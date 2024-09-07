bd4af24759f629857ec8f0a5e5d5bf06be388747
Boxy <nozakisola@gmail.com>
2024-09-07 18:48:51 -0500

    Dynamic element creation

--- script.js
+++ script.js
@@ -1,92 +1,116 @@
-const btn = document.getElementById("searchBtn");
 const err = document.getElementById("error");
-const imageElement = document.getElementById("icon");
-const aElement = document.getElementById("a");
+const mods = document.getElementById("mods");
+
+let imageElement;
+let aElement;
+let textElement;
+let descElement;
+
+let Name;
+let data;
+
+fetchData()
 
 async function fetchData() {
+  mods.innerHTML = "";
   try {
     const search = document.getElementById("searchBox").value.toLowerCase();
     const version = document.getElementById("versions").value;
     const loader = document.getElementById("loader").value;
 
     let catagory;
-    if (loader === 'All') {
-        catagory = ''
+    if (loader === "All") {
+      catagory = "";
+    } else {
+      catagory = ',["categories:' + loader + '"]';
     }
-    else {
-        catagory = ',["categories:' + loader + '"]'
-    }
-    
-    let versionStr
+
+    let versionStr;
 
     if (version == "any") {
-      versionStr = ""
+      versionStr = "";
     } else {
-      versionStr = ',["versions:' +
-      version +
-      '"'
+      versionStr = ',["versions:' + version + '"]';
     }
 
-    let url = 
-    "https://api.modrinth.com/v2/search?query=" +
-      search +
-      '&facets=[["project_type:mod"]' + catagory + 
-      versionStr +
-      ']&limit=1'
+    let queryStr;
+    if (search == "") {
+      queryStr = "";
+    } else {
+      queryStr = `query=${search}&`;
+    }
 
-      console.log(url)
+    let url = `https://api.modrinth.com/v2/search?${queryStr}facets=[["project_type:mod"]${catagory}${versionStr}]&limit=1`;
+
+    console.log(url);
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error("Cant find rescorse sry");
     }
-    console.log(url);
-    const data = await response.json();
+    data = await response.json();
     console.log(data.hits[0]);
     const image = data.hits[0].icon_url;
+    aElement = document.createElement("a");
+    imageElement = document.createElement("img");
+    imageElement.parentElement = aElement;
     imageElement.src = image;
     imageElement.style.display = "block";
     imageElement.alt = data.hits[0].title;
+    textElement = document.createElement("h4");
+    descElement = document.createElement("p");
+    descElement.innerHTML = data.hits[0].description;
     try {
-      let xversion1
-      if (!version == "any") {
-        xversion1 = '?game_versions=["' +
-          version +
-          '"]'
+      let xversion1;
+      if (version == "any") {
+        xversion1 = "";
       } else {
-        xversion1 = ""
+        xversion1 = '?game_versions=["' + version + '"]';
       }
-      let xurl = 
-      "https://api.modrinth.com/v2/project/" +
-        data.hits[0].project_id +
-        '/version' + xversion1
+      let xurl =`https://api.modrinth.com/v2/project/${data.hits[0].project_id}/version${xversion1}`
       const xresponse = await fetch(xurl);
+      console.log(xurl)
       if (!xresponse.ok) {
         throw new Error("Cant find rescorse sry");
       }
-      console.log(data.hits[0]);
-      const xdata = await xresponse.json();
+      let xdata = await xresponse.json();
       console.log(xdata);
       const xVersion = xdata[0]["files"][0]["url"];
+      mods.appendChild(aElement);
       aElement.href = xVersion;
-      err.innerHTML = data.hits[0].title + " for version " + xdata[0]["game_versions"][0];
-      console.log(xdata[0])
+      aElement.appendChild(imageElement);
+      Name =
+        `${data.hits[0].title} for version ${xdata[0]["game_versions"][0]}`;
+      textElement.innerHTML = Name;
+      err.style.display = "none";
+      aElement.appendChild(textElement);
+      aElement.appendChild(descElement);
+      console.log(xdata[0]);
     } catch (error) {
-        ConsoleError(error)
+      ConsoleError(error);
     }
   } catch (error) {
-    ConsoleError(error)
+    let error1 = error;
+    try {
+      if ((data.hits = [])) {
+        err.innerHTML = "No mods found";
+        err.style.display = "block";
+      } else {
+        ConsoleError(error1);
+      }
+    } catch (err) {
+      ConsoleError(error1);
+    }
   }
 }
 
 function handle(event) {
   if (event.key === "Enter") {
-    fetchData("1.20");
+    fetchData();
   }
 }
 
-
 function ConsoleError(error) {
-    err.innerHTML = "There are no mods found! Check the console if you want to know more";
-    console.error(error);
-    imageElement.style.display = "none";
+  err.style.display = "block";
+  err.innerHTML = `${error}`;
+  console.error(error);
 }
