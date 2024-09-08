d89b4f347649e2e6ef326297eaa725b3f22212d2
Boxy <nozakisola@gmail.com>
2024-09-07 23:56:10 -0500

    Reorganization

--- script.js
+++ script.js
@@ -9,30 +9,27 @@ let descElement;
 let Name;
 let data;
 
-fetchData()
+fetchData();
 
 async function fetchData() {
   mods.innerHTML = "";
   try {
+    //URL Setup
     const search = document.getElementById("searchBox").value.toLowerCase();
     const version = document.getElementById("versions").value;
     const loader = document.getElementById("loader").value;
-
     let catagory;
     if (loader === "All") {
       catagory = "";
     } else {
       catagory = ',["categories:' + loader + '"]';
     }
-
     let versionStr;
-
     if (version == "any") {
       versionStr = "";
     } else {
       versionStr = ',["versions:' + version + '"]';
     }
-
     let queryStr;
     if (search == "") {
       queryStr = "";
@@ -40,8 +37,8 @@ async function fetchData() {
       queryStr = `query=${search}&`;
     }
 
+    //GET Metadata
     let url = `https://api.modrinth.com/v2/search?${queryStr}facets=[["project_type:mod"]${catagory}${versionStr}]&limit=1`;
-
     console.log(url);
     const response = await fetch(url);
     if (!response.ok) {
@@ -49,45 +46,57 @@ async function fetchData() {
     }
     data = await response.json();
     console.log(data.hits[0]);
+
+    //GET Download
+
+    let xversion1;
+    if (version == "any") {
+      xversion1 = "";
+    } else {
+      xversion1 = `game_versions=["${version}"]`;
+    }
+
+    let loader1;
+    if (loader == "All") {
+      loader1 = ''
+    } else {
+      loader1 = `&loaders=["${loader}"]`
+    }
+    let xurl = `https://api.modrinth.com/v2/project/${data.hits[0].project_id}/version?${xversion1}${loader1}`;
+    const xresponse = await fetch(xurl);
+    console.log(xurl);
+    if (!xresponse.ok) {
+      throw new Error("Cant find rescorse sry");
+    }
+    let xdata = await xresponse.json();
+    console.log(xdata);
+    const xVersion = xdata[0]["files"][0]["url"];
     const image = data.hits[0].icon_url;
+    Name = `${data.hits[0].title} for version ${xdata[0]["game_versions"][0]}`;
+
+    //Setup Elements
     aElement = document.createElement("a");
     imageElement = document.createElement("img");
+    textElement = document.createElement("h4");
+    descElement = document.createElement("p");
+    aElement.href = xVersion;
+    err.style.display = "none";
+
+    //Append Items
+    mods.appendChild(aElement);
+    aElement.appendChild(textElement);
+    aElement.appendChild(descElement);
+    aElement.appendChild(imageElement);
+
+    //Images
     imageElement.parentElement = aElement;
     imageElement.src = image;
     imageElement.style.display = "block";
     imageElement.alt = data.hits[0].title;
-    textElement = document.createElement("h4");
-    descElement = document.createElement("p");
+
+    //Meta
     descElement.innerHTML = data.hits[0].description;
-    try {
-      let xversion1;
-      if (version == "any") {
-        xversion1 = "";
-      } else {
-        xversion1 = '?game_versions=["' + version + '"]';
-      }
-      let xurl =`https://api.modrinth.com/v2/project/${data.hits[0].project_id}/version${xversion1}`
-      const xresponse = await fetch(xurl);
-      console.log(xurl)
-      if (!xresponse.ok) {
-        throw new Error("Cant find rescorse sry");
-      }
-      let xdata = await xresponse.json();
-      console.log(xdata);
-      const xVersion = xdata[0]["files"][0]["url"];
-      mods.appendChild(aElement);
-      aElement.href = xVersion;
-      aElement.appendChild(imageElement);
-      Name =
-        `${data.hits[0].title} for version ${xdata[0]["game_versions"][0]}`;
-      textElement.innerHTML = Name;
-      err.style.display = "none";
-      aElement.appendChild(textElement);
-      aElement.appendChild(descElement);
-      console.log(xdata[0]);
-    } catch (error) {
-      ConsoleError(error);
-    }
+    textElement.innerHTML = Name;
   } catch (error) {
     let error1 = error;
     try {
